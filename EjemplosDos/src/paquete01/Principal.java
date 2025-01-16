7c999b88dae6c8aa82a529c5a53ca26e2b6ac898
abrahamx02 <georssj@gmail.com>
2025-01-16 11:59:31 -0500

    uso de clases en diferentes paquetes

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -9,6 +9,7 @@ import paquete02.DatoPersonal;
 import paquete03.DatosUbicacion;
 import paquete04.DatoAcademico;
 import paquete05.DatoFinal;
+import paquete02.DatoTrabajo;
 
 /**
  *
@@ -20,7 +21,9 @@ public class Principal {
         String nombre;
         String ciudad;
         String apellidoRetornado;
-        
+        String nombreEmpresa;
+        String direccionEmpresa;
+       
         double[] misNotas;
         double promedio;
         
@@ -29,16 +32,21 @@ public class Principal {
         ciudad = DatosUbicacion.obtenerCiudad();
         misNotas = DatoAcademico.obtenerNotas(4);
         promedio = DatoFinal.obtenerPromedio(misNotas);
+        nombreEmpresa = DatoTrabajo.nombreUs();
+        direccionEmpresa = DatoTrabajo.directionUs();
         
         
         System.out.printf("Los datos ingresados son:\n"
                 + "Nombre: %s\n"
                 + "Apellido: %s\n"
                 + "Ciudad: %s\n"
+                + "empresa :%s - Direccion : %s \n"
                 + "Promedio: %.2f\n",
                 nombre,
                 apellidoRetornado,
                 ciudad,
+                nombreEmpresa,
+                direccionEmpresa,
                 promedio);
     }
     
--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -9,8 +9,22 @@ package paquete02;
  *
  * @author reroes
  */
+import java.util.Scanner;
 public class DatoTrabajo {
     // método que permita preguntar al usuario el nombre de la empresa
-    
+    public static String nombreUs (){
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("ingrese el nombre de la empresa");
+        String nombreEmpresa = entrada.nextLine();
+        
+       return nombreEmpresa; 
+    }
     // método que permita preguntar al usuario la dirección de la empresa
+     public static String directionUs (){
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("ingrese la direccion de la empresa");
+        String directionEmpresa = entrada.nextLine();
+        
+       return directionEmpresa; 
+    }
 }
